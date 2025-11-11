1bcef5ea36b36bf1051047054bc695e34ab78739
Rita Zaarour <ritazaarour9@gmail.com>
2025-11-11 11:11:12 -0800

    added custom map

--- map.js
+++ map.js
@@ -10,7 +10,7 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoicml0YXphYXJvdXIiLCJhIjoiY21odTQ2enBnMHFkcTJxc
 // Initialize the map
 const map = new mapboxgl.Map({
   container: 'map', // ID of the div where the map will render
-  style: 'mapbox://styles/mapbox/light-v11', // Map style
+  style: 'mapbox://styles/ritazaarour/cmhu6fkv300e301regtsi4h5j', // Map style
   center: [-71.09415, 42.36027], // [longitude, latitude]
   zoom: 12, // Initial zoom level
   minZoom: 5, // Minimum allowed zoom
